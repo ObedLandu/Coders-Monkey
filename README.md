8f1aefd1c53be0af0c9cdf68943a7d0a45225780
obed landu <149078000+ObedLandu@users.noreply.github.com>
2024-10-10 16:58:11 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Coders Monkeys
+
+
+
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Seo from "@/ui/components/Seo";
+
+
+export default function Home() {
+  return (
+    <>
+     <Seo title="Coders Monkey" description="description..."/>
+    </>
+  );
+}
