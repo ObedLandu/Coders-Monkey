bcfdfcdbcfec0c266ebf22d1cc522f094de387a4
obed landu <149078000+ObedLandu@users.noreply.github.com>
2024-10-14 14:58:48 +0100

    second commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import Seo from "@/ui/components/Seo";
 
-
 export default function Home() {
   return (
     <>
      <Seo title="Coders Monkey" description="description..."/>
+     <h1>Les choses de la vie</h1>
     </>
   );
 }
